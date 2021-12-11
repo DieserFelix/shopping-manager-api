6bf74ff4d3923d109173572f05f5ee941b772ee9
Felix <felix2510@gmx.net>
2021-12-11 09:09:24 +0100

    Initial FastAPI setup

---
+++ app/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.openapi.utils import get_openapi
+import app.routers as routers
+
+app = FastAPI()
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+for router in routers.routers:
+    app.include_router(router)
+    
+app.openapi_schema = get_openapi(
+    title="Shopping Manager API",
+    version="1.0",
+    description="The Shopping Manager API maintains users, lists and products and their relations.",
+    routes=app.routes
+)
