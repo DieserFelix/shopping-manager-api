f5f69d5e485c8ff3d84699e513e62e9203c7b80d
Felix <felix2510@gmx.net>
2021-12-15 10:33:38 +0100

    Shopping List routes

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -4,5 +4,6 @@ from app.routers.users import users
 from app.routers.stores import stores
 from app.routers.categories import categories
 from app.routers.articles import articles
+from app.routers.lists import lists
 
-routers = [users, stores, categories, articles]
+routers = [users, stores, categories, articles, lists]
--- app/routers/lists.py
+++ app/routers/lists.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from typing import List
 from fastapi import APIRouter, Depends, HTTPException
 from starlette.responses import Response
-from app.db.models import User, Category
+from app.db.models import User, Category, ShoppingList, ShoppingListItem
 from app.lib import get_current_user, get_db, UserRoles
 import app.schemas as schemas
 from sqlalchemy.orm import Session
@@ -27,7 +27,7 @@ def read_lists(from_date: datetime = None, to_date: datetime = None, auth_user: 
             from_date = datetime.min
         if not to_date:
             to_date = datetime.max
-        lists = [entity for entity in auth_user.entities if entity.isList() and entity.updated_at >= from_date and entity.updated_at <= to_date]
+        lists = [list for list in auth_user.lists if list.updated_at >= from_date and list.updated_at <= to_date]
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     else:
@@ -44,34 +44,38 @@ def read_lists(from_date: datetime = None, to_date: datetime = None, auth_user: 
 )
 def read_list(list_id: int, auth_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
     try:
-        entity = Entity.get(list_id, auth_user, db)
-        if not entity.isList():
-            raise LookupError(f"No such entity: {list_id}")
+        list = ShoppingList.get(list_id, auth_user, db)
     except LookupError as e:
         raise HTTPException(status_code=404, detail=str(e))
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     else:
-        return entity
+        return list
 
 
+@lists.post(
+    "/",
+    response_model=schemas.List,
+    status_code=201,
+    responses={
+        201: dict(description="Shopping list <list_id>."),
+        400: dict(description="Input validation failed.", model=schemas.HTTPError),
+        404: dict(description="Shopping list <list_id> does not exist.", model=schemas.HTTPError)
+    }
+)
 def create_list(list: schemas.ListCreate, auth_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
     try:
-
-        current_list = Entity()
-        current_list.title = Entity.process_title(list.title)
+        current_list = ShoppingList()
+        current_list.title = ShoppingList.process_title(list.title)
         current_list.updated_at = datetime.utcnow()
-
-        current_entity_type = EntityType()
-        current_list_entity_type = ListEntityType()
-        current_list_entity_type.entity_type = current_entity_type
 
         if list.category_id is not None:
             category = Category.get(list.category_id, auth_user, db)
-            current_list_entity_type.category = category
+            current_list.category = category
+        current_list.finalized = False
 
-        db.add(current_entity_type)
-        db.add(current_list_entity_type)
+        current_list.username = auth_user.username
+
         db.add(current_list)
         db.commit()
     except ValueError as e:
@@ -85,17 +89,23 @@ def create_list(list: schemas.ListCreate, auth_user: User = Depends(get_current_
         return current_list
 
 
+@lists.put(
+    "/",
+    response_model=schemas.List,
+    responses={
+        200: dict(description="Shopping list <list_id>."),
+        400: dict(description="Input validation failed.", model=schemas.HTTPError),
+        404: dict(description="Shopping list <list_id> does not exist.", model=schemas.HTTPError)
+    }
+)
 def update_list(list: schemas.ListUpdate, auth_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
     try:
-        current_list = Entity.get(list.id, auth_user, db)
-        if not current_list.isList():
-            raise LookupError(f"No such entity: {list.id}")
-
+        current_list = ShoppingList.get(list.id, auth_user, db)
         if list.title is not None:
-            current_list.title = Entity.process_title(list.title)
+            current_list.title = ShoppingList.process_title(list.title)
         if list.category_id is not None:
             category = Category.get(list.category_id, auth_user, db)
-            current_list.entity_type.list_entity_type.category = category
+            current_list.category = category
         if list.finalized is not None:
             current_list.finalized = list.finalized
 
@@ -111,14 +121,24 @@ def update_list(list: schemas.ListUpdate, auth_user: User = Depends(get_current_
         return current_list
 
 
-# def delete_list(list_id: int, auth_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
-#     try:
-#         current_list = Entity.get(list_id, auth_user, db)
-#         if not current_list.isList():
-#             raise LookupError(f"No such entity: {list_id}")
+@lists.delete(
+    "/{list_id}",
+    status_code=204,
+    responses={
+        204: dict(description="Deleted shopping list <list_id>."),
+        404: dict(description="Shopping list <list_id> does not exist.", model=schemas.HTTPError)
+    }
+)
+def delete_list(list_id: int, auth_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+    try:
+        current_list = ShoppingList.get(list_id, auth_user, db)
 
-#         for child in current_list.children:
-#             db.delete(child)
-
-#         db.delete(current_list.entity_type)
-#         db.delete(current_list)
+        db.delete(current_list)
+        db.commit()
+    except LookupError as e:
+        raise HTTPException(status_code=404, detail=str(e))
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=500, detail=str(e))
+    else:
+        return Response(status_code=204)
