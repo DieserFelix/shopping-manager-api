87e93c86ad1159c23111b64192243b7ccdb6cf8b
Felix <felix2510@gmx.net>
2021-12-19 10:36:14 +0100

    Removed article/store lists from Store and Category schema

--- app/schemas/Store.py
+++ app/schemas/Store.py
@@ -15,23 +15,6 @@ class StoreUpdate(BaseModel):
 class Store(BaseModel):
     id: int
     name: str
-    articles: List[Any]
-
-    @validator("articles")
-    def validate_articles(cls, articles):
-        for i in range(len(articles)):
-            articles[i] = articles[i].id
-
-        return articles
 
     class Config:
         orm_mode = True
-
-        def schema_extra(schema: Dict[str, Any]) -> None:
-            schema["properties"]["articles"] = {
-                "title": "Articles",
-                "type": "array",
-                "items": {
-                    "type": "integer"
-                }
-            }
